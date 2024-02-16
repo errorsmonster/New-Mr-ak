9888a53414e896b6f5590c4e31d34f36a65c9bf9
errorsmonster <151611578+errorsmonster@users.noreply.github.com>
2024-02-16 12:27:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -57,12 +57,12 @@ async def stream_downloader(bot, query):
         file_id=file_id,
         caption=f_caption)
 
-    online = f"https://{ON_WATCH}/watch/{msg.id}?hash={get_hash(msg)}"
-    download = f"https://{ON_DWNLD}/{msg.id}?hash={get_hash(msg)}"
-    #page_link = f"https://{ON_WATCH}/watch/{msg.id}?hash={get_hash(msg)}"
-    #stream_link = f"https://{ON_DWNLD}/{msg.id}?hash={get_hash(msg)}"
-    #online = await get_shortlink(chat_id=msg.chat.id, link=page_link)
-    #download = await get_shortlink(chat_id=msg.chat.id, link=stream_link)
+    #online = f"https://{ON_WATCH}/watch/{msg.id}?hash={get_hash(msg)}"
+    #download = f"https://{ON_DWNLD}/{msg.id}?hash={get_hash(msg)}"
+    page_link = f"https://{ON_WATCH}/watch/{msg.id}?hash={get_hash(msg)}"
+    stream_link = f"https://{ON_DWNLD}/{msg.id}?hash={get_hash(msg)}"
+    online = await get_shortlink(chat_id=msg.chat.id, link=page_link)
+    download = await get_shortlink(chat_id=msg.chat.id, link=stream_link)
 
     await query.edit_message_reply_markup(
         reply_markup=InlineKeyboardMarkup(
